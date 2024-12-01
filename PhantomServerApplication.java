13071439589d63662bc97013b29a8cab2bb67c5b
Xiaojiu233 <RT_200@163.com>
2024-12-01 11:21:48 +0800

    开始代码基本框架搭建

---
+++ PhantomServerApplication.java
@@ -0,0 +1,16 @@
+
+//用于启动整个系统的应用类，项目的基础
+public class PhantomServerApplication {
+
+    //初始化系统与开启服务器
+    public static void main(String[] args) {
+        //1.初始化核心组件
+
+        //2.拓展jar包的读取与调用初始化方法
+
+        //3.IOC容器反射读取拓展程序
+
+        //4.服务器开机
+
+    }
+}
